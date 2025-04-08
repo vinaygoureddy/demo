510cc9f864e9d70427b567d4c975c7d9c173de87
vinay reddy <vinay.goureddy@gmail.com>
2025-04-07 18:20:59 -0700

    update login endpoint

--- src/main/java/com/example/demo/challenge/controller/BrowserChallengesController.java
+++ src/main/java/com/example/demo/challenge/controller/BrowserChallengesController.java
@@ -11,15 +11,13 @@ import com.example.demo.challenge.utils.JsonUtil;
 import io.micrometer.common.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "*")
 public class BrowserChallengesController {
 
     private final ChallengeService challengeService;
